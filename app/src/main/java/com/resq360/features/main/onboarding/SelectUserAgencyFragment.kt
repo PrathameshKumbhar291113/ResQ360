5a8a2a25ac52178cc030627e20d71a821afe8242
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2023-09-28 16:17:38 +0530

    ~~ onboarding flow is ready

--- app/src/main/java/com/resq360/features/main/onboarding/SelectUserAgencyFragment.kt
+++ app/src/main/java/com/resq360/features/main/onboarding/SelectUserAgencyFragment.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.resq360.R
 import com.resq360.databinding.FragmentSelectUserAgencyBinding
+import com.resq360.features.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SelectUserAgencyFragment : Fragment() {
+class SelectUserAgencyFragment : BaseFragment() {
     private lateinit var binding: FragmentSelectUserAgencyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,11 +33,46 @@ class SelectUserAgencyFragment : Fragment() {
 
     private fun setupUi() {
         binding.cardAgency.setOnClickListener {
-            findNavController().navigate(R.id.agencyLoginFragment)
+            enableNextButton()
+            binding.cardAgency.isChecked = true
+            binding.cardUser.isChecked = false
         }
         binding.cardUser.setOnClickListener {
-            findNavController().navigate(R.id.userLoginFragment)
+            enableNextButton()
+
+            binding.cardAgency.isChecked = false
+            binding.cardUser.isChecked = true
         }
+
+        binding.nextBtn.setOnClickListener {
+            enableDisableNextButton()
+        }
+    }
+
+    private fun enableDisableNextButton() {
+        if (binding.cardAgency.isChecked || binding.cardUser.isChecked){
+            enableNextButton()
+            navigateToSelectedFragment()
+        }else{
+            warningToast("Kindly Select One Role!!!")
+            disableNextButton()
+        }
+    }
+
+    private fun navigateToSelectedFragment() {
+        if (binding.cardUser.isChecked){
+            findNavController().navigate(R.id.userLoginFragment)
+        }else if (binding.cardAgency.isChecked){
+            findNavController().navigate(R.id.agencyLoginFragment)
+        }
+    }
+
+    private fun disableNextButton() {
+        binding.nextBtn.setBackgroundDrawable((ContextCompat.getDrawable(requireContext(), R.drawable.shape_layer_button_disable)))
+    }
+
+    private fun enableNextButton() {
+        binding.nextBtn.setBackgroundDrawable((ContextCompat.getDrawable(requireContext(), R.drawable.shape_layer_button)))
     }
 
 }

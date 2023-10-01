66ef6899607628ca0069cddefd0a02db5df55c94
Prathamesh Kumbhar <99268055+prathameshkumbhar291113@users.noreply.github.com>
2023-10-01 14:10:06 +0530

    ~~added a function to the ResQ360Application.kt.

--- app/src/main/java/com/resq360/ResQ360Application.kt
+++ app/src/main/java/com/resq360/ResQ360Application.kt
@@ -1,11 +1,16 @@
 package com.resq360
 
 import android.app.Application
+import android.content.res.Configuration
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class ResQ360Application : Application(){
     override fun onCreate() {
         super.onCreate()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
     }
 }

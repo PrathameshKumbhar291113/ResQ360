fb6dee50871a299cafaef88ea2d986fddedbf2d1
Prachi <prachi.jamdade2390@gmail.com>
2023-10-05 21:07:32 +0530

    After submit screen added

--- app/src/main/java/com/resq360/features/main/auth/agency/register/SubmitFragment.kt
+++ app/src/main/java/com/resq360/features/main/auth/agency/register/SubmitFragment.kt
@@ -1,42 +1,41 @@
 package com.resq360.features.main.auth.agency.register
 
 import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.resq360.R
-import com.resq360.databinding.FragmentAgencyRegisterStep4Binding
+import com.resq360.databinding.FragmentAgencyRegisterStep1Binding
+import com.resq360.databinding.FragmentSubmitBinding
 
-class AgencyRegisterFragmentStep4 : Fragment() {
-    private lateinit var binding: FragmentAgencyRegisterStep4Binding
+class SubmitFragment : Fragment() {
+
+    private lateinit var binding: FragmentSubmitBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        navigateToHome()
+    }
+
+    private fun navigateToHome() {
+        Handler().postDelayed(Runnable {
+            findNavController().navigate(R.id.homeFragment)
+        }, 3000)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View{
-        binding = FragmentAgencyRegisterStep4Binding.inflate(inflater, container, false)
+    ): View? {
+        binding = FragmentSubmitBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupUi()
-        setupObserver()
     }
 
-    private fun setupObserver() {
-
-    }
-    private fun setupUi() {
-
-        binding.submitButton.setOnClickListener {
-            findNavController().navigate(R.id.submitFragment)
-        }
-    }
 }

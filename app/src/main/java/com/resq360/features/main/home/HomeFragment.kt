2492e232749fb744f1221ab2f618b9a5bd44155a
Prathamesh Kumbhar <99268055+prathameshkumbhar291113@users.noreply.github.com>
2023-10-06 03:11:09 +0530

    [Code Changes]: Add get user location logic.

--- app/src/main/java/com/resq360/features/main/home/HomeFragment.kt
+++ app/src/main/java/com/resq360/features/main/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.resq360.R
 import com.resq360.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,6 +14,7 @@ class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requireActivity().window.statusBarColor = requireActivity().getColor(R.color.lightish_teal_green)
     }
 
     override fun onCreateView(

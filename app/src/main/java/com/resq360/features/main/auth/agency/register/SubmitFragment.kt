1db86b5cee841bca5d048ba5a9ed266c71b74e0c
Prachi <94190717+Prachi-Jamdade@users.noreply.github.com>
2023-10-06 21:18:35 +0530

    Prachi/resq360 (#9)
    * Basic flow and UI of registration screens
    
    * Complete Registration screen 2 UI
    
    * ~~added comment.
    
    * removed comment
    
    * ~~ do the changes stated in the ui screens.
    
    * Make changes to registration 2 UI screen
    
    * Basic changes
    
    * After submit screen added
    
    * Add auto detect location and upload document responsiveness
    
    * Validations to Register Step 1 screen
    
    ---------
    
    Co-authored-by: PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>

--- app/src/main/java/com/resq360/features/main/auth/agency/register/SubmitFragment.kt
+++ app/src/main/java/com/resq360/features/main/auth/agency/register/SubmitFragment.kt
@@ -1,45 +1,41 @@
 package com.resq360.features.main.auth.agency.register
 
 import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.resq360.R
-import com.resq360.databinding.FragmentAgencyRegisterStep2Binding
-import dagger.hilt.android.AndroidEntryPoint
+import com.resq360.databinding.FragmentAgencyRegisterStep1Binding
+import com.resq360.databinding.FragmentSubmitBinding
 
-@AndroidEntryPoint
-class AgencyRegisterFragmentStep2 : Fragment() {
-    private lateinit var binding: FragmentAgencyRegisterStep2Binding
+class SubmitFragment : Fragment() {
+
+    private lateinit var binding: FragmentSubmitBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        navigateToHome()
+    }
+
+    private fun navigateToHome() {
+        Handler().postDelayed(Runnable {
+            findNavController().navigate(R.id.homeFragment)
+        }, 3000)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentAgencyRegisterStep2Binding.inflate(inflater, container, false)
+        binding = FragmentSubmitBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupUi()
-        setupObserver()
-    }
-
-    private fun setupObserver() {
-
-    }
-    private fun setupUi() {
-
-        binding.nextButton.setOnClickListener {
-            findNavController().navigate(R.id.agencyRegisterFragmentStep3)
-        }
     }
 
 }
